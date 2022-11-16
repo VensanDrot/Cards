695ceeb5ad8035036a8d7898c1f34524ed56e9fc
VensanDrot <vensandrot@gmail.com>
2022-11-16 15:37:33 +0500

    Add react icons

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import styles from "./index.module.css"
-import Link from 'next/link'
+import React from "react";
+import styles from "./index.module.css";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <div className={styles.nav_container}>
-      <Link href="/" className='btn btn-primary'>Main Page</Link>
-      <Link href="Login" className='btn btn-primary'>Login Page</Link>
+      <Link href='/' className='btn btn-primary'>
+        Main Page
+      </Link>
+      <Link href='Login' className='btn btn-primary'>
+        Login Page
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
