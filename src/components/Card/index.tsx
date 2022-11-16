ae712269075c08955a1578a90e942fd4c4f64d9a
VensanDrot <vensandrot@gmail.com>
2022-11-16 18:16:33 +0500

    Final changes

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,26 +5,54 @@ import { ImCross } from "react-icons/im";
 import { AiOutlineSave } from "react-icons/ai";
 
 const Card = () => {
+  //elememts
   const Lorem =
-    "ipsum dolor sit amet consectetur adipisicing elit. Sunt harum accusamus iure temporibus quas quasi voluptate quaerat quae, explicabo ducimus autem consectetur aliquam velit praesentium ut cupiditate dolore, similique rem!";
+    "HEE adipisicing elit. Sunt harum accusamus iure temporibus quas quasi voluptate quaerat quae, explicabo ducimus autem consectetur aliquam velit praesentium ut cupiditate dolore, similique rem!";
   const [check, SetCheck] = useState(false);
   const [edit, SetEdit] = useState(false);
-  const [header, SetHeader] = useState(Lorem);
-  const [body, SetBody] = useState(Lorem);
+  const [header, SetHeader] = useState<string | undefined>(Lorem);
+  const [body, SetBody] = useState<string | undefined>(Lorem);
+  const [Oldheader, SetOldHeader] = useState<string | undefined>();
+  const [Oldbody, SetOldBody] = useState<string | undefined>();
 
+  //checker
   const checkhandler = () => {
     SetCheck(!check);
   };
 
+  //Start the edition proccess
   const edithandler = () => {
     SetEdit(!edit);
+    SetOldBody(body);
+    SetOldHeader(header);
     if (check) {
       SetCheck(!check);
     }
+    if (edit) {
+      console.log("here");
+      SetBody(Oldbody);
+      SetHeader(Oldheader);
+    }
   };
 
+  //chose the icon
   const replacer = () => {
     return edit ? <ImCross /> : <BsPencilSquare />;
+  };
+
+  //header text change handle
+  const headerhandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    SetHeader(e.target.value);
+  };
+
+  //body text change handle
+  const bodyhandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    SetBody(e.target.value);
+  };
+
+  //save changes
+  const save = () => {
+    SetEdit(!edit);
   };
 
   return (
@@ -33,16 +61,16 @@ const Card = () => {
         {replacer()}
       </button>
       <label>Header:</label>
-      <textarea className='dis_input' value={Lorem} readOnly={!edit}></textarea>
+      <textarea className='dis_input' onChange={(e) => headerhandler(e)} value={header} readOnly={!edit}></textarea>
       <label>Body:</label>
-      <textarea className='dis_input' value={Lorem} readOnly={!edit}></textarea>
+      <textarea className='dis_input' onChange={(e) => bodyhandler(e)} value={body} readOnly={!edit}></textarea>
       <div className={`${edit ? styles.hidden : styles.smth} ${styles.card_but_container}`}>
         <label>Color Change: </label>
         <input type='checkbox' name='check' value='true' onChange={checkhandler} checked={edit ? false : check} />
       </div>
 
       <div className={`${!edit && styles.hidden} ${styles.card_but_container}`}>
-        <button className={styles.save} style={{ fontSize: "40px" }}>
+        <button className={`${styles.save} ${styles.bigger}`} onClick={save}>
           <AiOutlineSave />
         </button>
       </div>
