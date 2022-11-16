5a5d5ff1d766f56d191b5c8413be7bfd625251b0
VensanDrot <vensandrot@gmail.com>
2022-11-16 13:54:27 +0500

    card color change on checkbox

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './index.module.css'
 
 
 const index = () => {
+  const Lorem = "ipsum dolor sit amet consectetur adipisicing elit. Sunt harum accusamus iure temporibus quas quasi voluptate quaerat quae, explicabo ducimus autem consectetur aliquam velit praesentium ut cupiditate dolore, similique rem!"
+  const [check, SetCheck] = useState(false);
+  const checkhandler = () => {
+    
+    SetCheck(!check);
+  }
+
+
   return (
-    <div className={styles.card_container}>
+    <div className={ `${!check? styles.smth  : styles.card_cont_active} ${styles.card_container} `} >
     <label>Header:</label>
-    <textarea className='dis_input' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, iusto distinctio aliquam dolores tempore libero vero, fuga earum quod expedita saepe doloribus odio possimus nam perspiciatis, quam iste! Corporis, odio?</textarea>
+    <textarea className='dis_input' id='header' defaultValue={Lorem}></textarea>
     <label>Body:</label>
-    <textarea className='dis_input' >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis aut voluptas labore reiciendis, neque a assumenda repudiandae quae eaque vitae consequatur minima deserunt exercitationem, reprehenderit maiores! Quidem, doloremque nemo! Porro.</textarea>
+    <textarea className='dis_input' id='body' defaultValue={Lorem}></textarea>
+    <div className={styles.card_but_container}>
+      <label>Color Change: </label>
+      <input type="checkbox" name='check' value="true" onChange={() => checkhandler()} />
     </div>
+  
+    </div>
+
   )
 }
 
