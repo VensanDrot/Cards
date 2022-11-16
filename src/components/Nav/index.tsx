dd5809adc94381699a7bc1d1211da2fc4421d4e4
VensanDrot <vensandrot@gmail.com>
2022-11-16 09:47:33 +0500

    First one

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./index.module.css"
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <div className={styles.nav_container}>
+      <Link href="/" className='btn btn-primary'>Main Page</Link>
+      <Link href="Login" className='btn btn-primary'>Login Page</Link>
+    </div>
+  )
+}
+
+export default Nav
